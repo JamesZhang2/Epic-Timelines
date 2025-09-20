08b5891baf3a5b664211cc6f741120abd0520918
JamesZhang2 <jameszhang2019@outlook.com>
2025-09-20 13:25:16 -0700

    Add input field for uploading files
    Reads an ics file and prints its content to console.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Epic Timelines
 
 This project is inspired by the Jira timelines for Epics. It allows you to import your calendar files (.ics) and track how various activities (the "Epics" in your personal life) appear on your calendars over time.
+
+## Dependencies
+
+- ical.js: `npm install ical.js`
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile] = useState<File | undefined>();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const fileInput = document.getElementById("file-input");
+
+  function handleFileUpload(e: React.ChangeEvent<HTMLInputElement>) {
+    const fileList = e.target.files;
+    if (!fileList) {
+      return;
+    }
+
+    const uploadedFile: File = fileList[0];
+    setFile(fileList[0]);
+    const reader = new FileReader();
+    reader.onload = () => {
+      const result = reader.result as string;
+      const parsed = parseICS(result);
+      console.log(parsed);
+    }
+
+    reader.readAsText(uploadedFile);
+  }
+
+  function parseICS(s: string) {
+    // TODO
+    return s;
+  }
+
+  return <>
+    <h1>Epic Timelines</h1>
+    <input type="file" id="file-input" accept=".ics" onChange={handleFileUpload} />
+  </>;
 }
 
 export default App
