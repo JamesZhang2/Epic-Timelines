01985af6b3a8dd0e018a2ea0588b995844daad30
JamesZhang2 <jameszhang2019@outlook.com>
2025-09-20 18:53:40 -0700

    Put time buckets in the first row of the table

--- src/Timelines.tsx
+++ src/Timelines.tsx
@@ -20,12 +20,25 @@ function Timelines({ events }: TimelinesProps) {
         timeBuckets.push({
             start: new Date(curDate),
             end: end
-        })
+        });
         curDate.setDate(curDate.getDate() + 1);
     }
     console.log(timeBuckets);
 
-    return <pre>Events: {JSON.stringify(events, null, 2)}</pre>
+    return <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Epics</th>
+                    {timeBuckets.map((bucket) => <th key={JSON.stringify(bucket)}>{(bucket.start.getMonth() + 1) + "/" + bucket.start.getDate()}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+        <pre>Events: {JSON.stringify(events, null, 2)}</pre>
+    </div>;
 }
 
 export default Timelines;
