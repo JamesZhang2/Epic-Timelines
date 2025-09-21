d685348a9f83d34e226d056ba0d49c3c68a9dbae
JamesZhang2 <jameszhang2019@outlook.com>
2025-09-20 18:38:42 -0700

    Create time buckets in Timelines

--- src/Timelines.tsx
+++ src/Timelines.tsx
@@ -1,10 +1,28 @@
 import type { CalendarEvent } from './App';
 
 type TimelinesProps = {
-    events: CalendarEvent[],
+    events: CalendarEvent[];
+}
+
+type TimeBucket = {
+    start: Date;
+    end: Date;
 }
 
 function Timelines({ events }: TimelinesProps) {
+    const startDate = new Date("2025-09-22T00:00:00Z");
+    const endDate = new Date("2025-09-28T00:00:00Z");
+    const timeBuckets: TimeBucket[] = [];
+    let curDate = startDate;
+    while (curDate <= endDate) {
+        timeBuckets.push({
+            start: new Date(curDate),
+            end: new Date(curDate.getDate() + 1)
+        })
+        curDate.setDate(curDate.getDate() + 1);
+    }
+    console.log(timeBuckets);
+
     return <pre>Events: {JSON.stringify(events, null, 2)}</pre>
 }
 
