0b6eff731f2528af70f14a455297ceeec56ee4a1
JamesZhang2 <jameszhang2019@outlook.com>
2025-09-20 19:05:42 -0700

    Factor out generateTimeBuckets function

--- src/Timelines.tsx
+++ src/Timelines.tsx
@@ -12,17 +12,7 @@ type TimeBucket = {
 function Timelines({ events }: TimelinesProps) {
     const startDate = new Date("2025-09-22T00:00:00");
     const endDate = new Date("2025-09-28T00:00:00");
-    const timeBuckets: TimeBucket[] = [];
-    let curDate = startDate;
-    while (curDate <= endDate) {
-        let end = new Date(curDate);
-        end.setDate(end.getDate() + 1);
-        timeBuckets.push({
-            start: new Date(curDate),
-            end: end
-        });
-        curDate.setDate(curDate.getDate() + 1);
-    }
+    const timeBuckets: TimeBucket[] = generateTimeBuckets(startDate, endDate);
     console.log(timeBuckets);
 
     return <div>
@@ -39,6 +29,24 @@ function Timelines({ events }: TimelinesProps) {
         </table>
         <pre>Events: {JSON.stringify(events, null, 2)}</pre>
     </div>;
+}
+
+/**
+ * Generates time buckets based on the start date and end date. Each time bucket interval is 1 day.
+ */
+function generateTimeBuckets(startDate: Date, endDate: Date): TimeBucket[] {
+    const timeBuckets = [];
+    let curDate = startDate;
+    while (curDate <= endDate) {
+        let end = new Date(curDate);
+        end.setDate(end.getDate() + 1);
+        timeBuckets.push({
+            start: new Date(curDate),
+            end: end
+        });
+        curDate.setDate(curDate.getDate() + 1);
+    }
+    return timeBuckets;
 }
 
 export default Timelines;
