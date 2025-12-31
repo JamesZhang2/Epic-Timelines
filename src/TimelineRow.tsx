1aa34ea8fadd08f764bcb053c469fdbaff074d2f
JamesZhang2 <jameszhang2019@outlook.com>
2025-12-31 10:57:26 -0800

    [Refactor] Move TimelineRow to a separate file

---
+++ src/TimelineRow.tsx
@@ -0,0 +1,46 @@
+import type { Epic } from "./EpicTimelines";
+import { colorToRGB, rgbToColor } from "./Util";
+
+type TimelineRowProps = {
+  epicBucketHours: Map<string, number[]>;
+  epic: Epic;
+  onEpicClick: () => void;
+}
+
+/** Represents a timeline for an Epic, which is a row in the Timelines table. */
+function TimelineRow({ epicBucketHours, epic, onEpicClick }: TimelineRowProps) {
+  const cells = [<th className="epic-name-cell" onClick={onEpicClick}>{epic.name}</th>];
+
+  const epicHours = epicBucketHours.get(epic.name);
+
+  if (!epicHours) {
+    throw new Error("epicBucketHours does not contain the name of the Epic")
+  }
+
+  const maxHours = Math.max(...epicHours);
+
+  for (const [i, hours] of epicHours.entries()) {
+    const cellColor = computeCellColor(hours, maxHours, epic.color);
+    const style = { backgroundColor: cellColor };
+    cells.push(<td key={i}><div className="colored-cell" style={style}></div></td>);
+  }
+  return <tr>{cells}</tr>;
+}
+
+/**
+ * Compute the color for a cell based on the number hours in this bucket,
+ * the max number of hours, and the color of the Epic.
+ */
+function computeCellColor(hours: number, maxHours: number, epicColor: string): string {
+  if (hours === 0) {
+    return "#ffffff";
+  }
+  const rgb = colorToRGB(epicColor);
+  const factor = Math.max(hours / maxHours, 0.25);
+  const newR = Math.floor(255 - (255 - rgb[0]) * factor);
+  const newG = Math.floor(255 - (255 - rgb[1]) * factor);
+  const newB = Math.floor(255 - (255 - rgb[2]) * factor);
+  return rgbToColor(newR, newG, newB);
+}
+
+export default TimelineRow;
--- src/Timelines.tsx
+++ src/Timelines.tsx
@@ -1,13 +1,7 @@
-import { colorToRGB, rgbToColor } from "./Util";
 import type { Epic, TimeBucket } from "./EpicTimelines";
+import TimelineRow from "./TimelineRow";
 import EpicDetails from "./EpicDetails";
 import React from "react";
-
-type TimelineRowProps = {
-  epicBucketHours: Map<string, number[]>;
-  epic: Epic;
-  onEpicClick: () => void;
-}
 
 type TimelinesProps = {
   epics: Epic[];
@@ -45,42 +39,6 @@ function Timelines({ epics, timeBuckets, epicBucketHours, selectedEpic, onEpicCl
       </tbody>
     </table>
   );
-}
-
-/** Represents a timeline for an epic, which is a row in the Timelines table. */
-function TimelineRow({ epicBucketHours, epic, onEpicClick }: TimelineRowProps) {
-  const cells = [<th className="epic-name-cell" onClick={onEpicClick}>{epic.name}</th>];
-
-  const epicHours = epicBucketHours.get(epic.name);
-
-  if (!epicHours) {
-    throw new Error("epicBucketHours does not contain the name of the Epic")
-  }
-
-  const maxHours = Math.max(...epicHours);
-
-  for (const [i, hours] of epicHours.entries()) {
-    const cellColor = computeCellColor(hours, maxHours, epic.color);
-    const style = { backgroundColor: cellColor };
-    cells.push(<td key={i}><div className="colored-cell" style={style}></div></td>);
-  }
-  return <tr>{cells}</tr>;
-}
-
-/**
- * Compute the color for a cell based on the number hours in this bucket,
- * the max number of hours, and the color of the Epic.
- */
-function computeCellColor(hours: number, maxHours: number, epicColor: string): string {
-  if (hours === 0) {
-    return "#ffffff";
-  }
-  const rgb = colorToRGB(epicColor);
-  const factor = Math.max(hours / maxHours, 0.25);
-  const newR = Math.floor(255 - (255 - rgb[0]) * factor);
-  const newG = Math.floor(255 - (255 - rgb[1]) * factor);
-  const newB = Math.floor(255 - (255 - rgb[2]) * factor);
-  return rgbToColor(newR, newG, newB);
 }
 
 export default Timelines;
