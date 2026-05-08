5bec10987b8382602746c6c28fce49e6f91de0b4
JamesZhang2 <jameszhang2019@outlook.com>
2026-05-07 21:19:04 -0700

    [Refactor] Rewrite timeline header code, factor out formatDate

--- src/TimelineHeader.tsx
+++ src/TimelineHeader.tsx
@@ -5,28 +5,25 @@ type TimelineHeaderProps = {
   timeBuckets: TimeBucket[];
 };
 
+function formatDate(date: Date, showYear: boolean) {
+  if (showYear) {
+    return date.getMonth() + 1 + "/" + date.getDate() + "/" + (date.getFullYear() % 100);
+  } else {
+    return date.getMonth() + 1 + "/" + date.getDate();
+  }
+}
+
 function TimelineHeader({ timeBuckets }: TimelineHeaderProps) {
   const headerCells = [];
   headerCells.push(<th>Epics</th>);
   for (let i = 0; i < timeBuckets.length; i++) {
-    if (i > 0 && timeBuckets[i].start.getFullYear() === timeBuckets[i - 1].start.getFullYear()) {
-      headerCells.push(
-        <th key={timeBuckets[i].start.toISOString()}>
-          {timeBuckets[i].start.getMonth() + 1 + "/" + timeBuckets[i].start.getDate()}
-        </th>,
-      );
-    } else {
-      headerCells.push(
-        <th key={timeBuckets[i].start.toISOString()}>
-          {timeBuckets[i].start.getMonth() +
-            1 +
-            "/" +
-            timeBuckets[i].start.getDate() +
-            "/" +
-            (timeBuckets[i].start.getFullYear() % 100)}
-        </th>,
-      );
-    }
+    const showYear =
+      i === 0 || timeBuckets[i].start.getFullYear() !== timeBuckets[i - 1].start.getFullYear();
+    headerCells.push(
+      <th key={timeBuckets[i].start.toISOString()}>
+        {formatDate(timeBuckets[i].start, showYear)}
+      </th>,
+    );
   }
   headerCells.push(<th>Total</th>);
   return (
