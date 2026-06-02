3ab8d0a96c9e261627616d1dbe754080d7db7065
JamesZhang2 <jameszhang2019@outlook.com>
2026-06-01 20:56:02 -0700

    Split out the event util methods from BucketUtil

--- src/BucketUtil.ts
+++ src/BucketUtil.ts
@@ -1,17 +1,9 @@
 /** This file contains helper functions for dealing with time buckets. */
 
 import { computeOverlapHours, hasNontrivialOverlap } from "./Util";
+import { epicMatchesEvent } from "./EventUtil";
 import type { CalendarEvent } from "./ICSParser";
 import type { TimeBucket, BucketedEvents, Epic } from "./EpicTimelines";
-
-const ALL_DAY_EVENT_MIN_DURATION_MS = 24 * 60 * 60 * 1000;
-
-/** Filters out events whose time span is at least 24 hours. */
-export function filterOutAllDayEvents(events: CalendarEvent[]): CalendarEvent[] {
-  return events.filter(
-    (event) => event.end.getTime() - event.start.getTime() < ALL_DAY_EVENT_MIN_DURATION_MS,
-  );
-}
 
 /**
  * @returns a list of BucketedEvents, one for each bucket in buckets.
@@ -65,24 +57,6 @@ export function computeEpicBucketHours(
     result.set(epic.name, epicHours);
   }
   return result;
-}
-
-/**
- * @returns true if the Epic matches the event (taking into account case sensitivity
- * and which fields to match), false otherwise.
- */
-export function epicMatchesEvent(epic: Epic, event: CalendarEvent) {
-  const regex = new RegExp(epic.keyword, epic.caseSensitive ? "" : "i"); // i: ignore case flag
-  if (epic.matchTitle && regex.test(event.title)) {
-    return true;
-  }
-  if (epic.matchDescription && event.description !== undefined && regex.test(event.description)) {
-    return true;
-  }
-  if (epic.matchLocation && event.location !== undefined && regex.test(event.location)) {
-    return true;
-  }
-  return false;
 }
 
 /**
---
+++ src/EventUtil.ts
@@ -0,0 +1,31 @@
+/** This file contains helper functions for dealing with calendar events. */
+
+import type { Epic } from "./EpicTimelines";
+import type { CalendarEvent } from "./ICSParser";
+
+const ALL_DAY_EVENT_MIN_DURATION_MS = 24 * 60 * 60 * 1000;
+
+/** Filters out events whose time span is at least 24 hours. */
+export function filterOutAllDayEvents(events: CalendarEvent[]): CalendarEvent[] {
+  return events.filter(
+    (event) => event.end.getTime() - event.start.getTime() < ALL_DAY_EVENT_MIN_DURATION_MS,
+  );
+}
+
+/**
+ * @returns true if the Epic matches the event (taking into account case sensitivity
+ * and which fields to match), false otherwise.
+ */
+export function epicMatchesEvent(epic: Epic, event: CalendarEvent) {
+  const regex = new RegExp(epic.keyword, epic.caseSensitive ? "" : "i"); // i: ignore case flag
+  if (epic.matchTitle && regex.test(event.title)) {
+    return true;
+  }
+  if (epic.matchDescription && event.description !== undefined && regex.test(event.description)) {
+    return true;
+  }
+  if (epic.matchLocation && event.location !== undefined && regex.test(event.location)) {
+    return true;
+  }
+  return false;
+}
